bb6213e9a7a674188264a14ef63a87851a00bb05
johansen98 <61602808+johansen98@users.noreply.github.com>
2022-02-07 12:13:24 +0100

    done

--- js/question-1.js
+++ js/question-1.js
@@ -1,1 +1,8 @@
+// Question 1
+// Convert the function below to an arrow function:
 
+// function getRemainder(a,b) {
+// return a % b;
+// }
+
+let getRemainder = (a,b) => a % b;
--- js/question-2.js
+++ js/question-2.js
@@ -1,1 +1,44 @@
+// Question 2
+// Make a call to the Rawg API.
 
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=27ee92531f1c41e7a6885e0fedf0ecba";
+
+const getGames = async () => {
+
+    const response = await fetch(url);
+
+    const result = await response.json();
+    
+    return result.results;
+
+}
+
+ getGames().then((result)=>{
+    const games = result.slice(0,8);
+     displayGames(games);
+ })
+
+const displayGames = (games) =>{
+    const containerDiv = document.getElementById("container");
+
+    games.forEach(game => {
+        const div = document.createElement("div");
+        const heading = document.createElement("h2");
+        const rating = document.createElement("p");
+        const tags = document.createElement("p");
+
+        heading.innerHTML = game.name;
+        rating.innerHTML = "Rating: " + game.rating;
+        tags.innerHTML = "Number Of Tags: " + game.tags.length;
+
+        div.appendChild(heading);
+        div.appendChild(rating);
+        div.appendChild(tags);
+        containerDiv.appendChild(div);
+       
+
+
+    });
+
+
+}
